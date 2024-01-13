09c6f6dfba04822f2b3573f11c5c81bfe02e5236
No Need <77963254+G8ur@users.noreply.github.com>
2024-01-13 11:01:05 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import { Routes, Route , Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {Navbar , Home , Exchanges , Cryptocurrencies , CryptoDetails , News } from './components';
+import './App.css';
 const App = () => {
   return (
-    <div>
-      <h1>Crypto News</h1>
+    <div className='app'>
+        <div className='navbar'>
+            <Navbar />
+
+        </div>
+        <div className='main'>
+            <Layout>
+                <div className='routes'>
+                  <Routes>
+                    <Route path="/" exact element={<Home/>} />
+                    <Route path="/exchanges" exact element={<Exchanges/>} />
+                    <Route path="/cryptocurrencies" exact element={<Cryptocurrencies/>} />
+                    <Route path="/crypto/:coinId" exact element={<CryptoDetails/>} />
+                    <Route path="/news" exact element={<News/>} />
+                  </Routes>
+                </div>
+            </Layout>
+
+        
+        <div className='footer' >
+          <Typography.Title level={5} style={{color:'white', textAlign: 'center' }}>
+            CryptoNews <br/>
+            All rights reserved
+          </Typography.Title>
+          <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+          </Space>
+
+        </div>
+        </div>
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  return (
+    <div>
+Home
+    </div>
+  )
+}
+
+export default Home

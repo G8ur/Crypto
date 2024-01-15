7b040c4fd0d1f72f0d6856b3888d0d8136f2ee1c
No Need <77963254+G8ur@users.noreply.github.com>
2024-01-15 19:23:25 +0530

    news done

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,21 +3,53 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const {text, title} = Typography;
+const {Text, Title} = Typography;
 const {Option} = Select;
 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 
 const News = ({simplified}) => {
-  const count = simplified ? 10 : 100;
+  const count = simplified ? 6 : 12;
   // const {data:cryptoNews} = useGetCryptoNewsQuery({newsCategory:'Cryptocurrency',count:simplified ? 10: 100});
-  const {data:cryptoNews, isFetching} = useGetCryptoNewsQuery(count);
+  const {data:cryptoNews} = useGetCryptoNewsQuery(count);
+
   console.log(cryptoNews)
-  if (isFetching) return "Loading";
+  
+  if(!cryptoNews?.data) return 'Loading...'
+
   return (
-    <div>
-      News 
-    </div>
+    <Row gutter={[24,24]}>
+      {cryptoNews.data.map((news , i)=>(
+        <Col xs={24} sm={12} lg={8} key={i} >
+          <Card hoverable className='news-card' >
+            <a href={news.url} target='_blank' rel='noreferrer'>
+              <div className='news-image-container'>
+                <Title className="news-title" level={4} >
+                  {news.title} 
+{/* for news here the news */}
+
+                </Title>
+                <img src={news?.thumbnail|| demoImage} alt="news" width="50" height="60" />
+              </div>
+              <p>
+              {news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  {/* <Avatar src={news.provider[0]?.thumbnail || demoImage} alt="" />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text> */}
+                </div>
+                <Text>{moment(news.createdAt).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+
+      ))}
+
+    </Row>
+    
   )
 }
 
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -4,8 +4,8 @@ const cryptoNewsHeaders = {
 //   "X-BingApis-SDK": "true",
 //   "X-RapidAPI-Key": "27216bf07fmshdee129fa8936a8ep140b38jsn4c157ef95359",
 //   "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
-'X-RapidAPI-Key': '27216bf07fmshdee129fa8936a8ep140b38jsn4c157ef95359',
-'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com'
+ 'X-RapidAPI-Key': '27216bf07fmshdee129fa8936a8ep140b38jsn4c157ef95359',
+ 'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com'
 };
 
 const baseUrl = 'https://cryptocurrency-news2.p.rapidapi.com/v1';
@@ -20,7 +20,7 @@ export const cryptoNewsApi = createApi({
     endpoints:(builder) =>({
         getCryptoNews: builder.query({
             //  query: ({newsCategory , count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`) 
-            query: (count) => createRequest(`/bsc?limit=${count}`)
+             query: (count) => createRequest(`/bsc?limit=${count}`)
         })
     })
 })

8a71771a39c7c80fe47fca71da34ce6f97ab7be2
No Need <77963254+G8ur@users.noreply.github.com>
2024-01-16 09:14:51 +0530

    starting with crytpodetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,20 +1,74 @@
-import React, { useState } from 'react';
-import HTMLReactParser from 'html-react-parser';
-import { useParams } from 'react-router-dom';
-import millify from 'millify';
-import { Col, Row, Typography, Select } from 'antd';
-import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
-  const {coinId} = useParams()
-  return (
-    <div>
-      Crytpto Details{coinId}
-    </div>
-  )
-}
+  const { coinId } = useParams();
+  const [timePeriod, setTimePeriod] = useState("7d");
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const cryptodetails = data?.data?.coin
+  console.log(data);
+  if (isFetching) return 'Loadingg....' 
+  // it is imp as important to load data
 
-export default CryptoDetails
+  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+  const stats = [
+    { title: 'Price to USD', value: `$ ${cryptodetails?.price && millify(cryptodetails?.price)}`, icon: <DollarCircleOutlined /> },
+    { title: 'Rank', value: cryptodetails?.rank, icon: <NumberOutlined /> },
+    { title: '24h Volume', value: `$ ${cryptodetails?.volume && millify(cryptodetails?.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: 'Market Cap', value: `$ ${cryptodetails?.marketCap && millify(cryptodetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
+    { title: 'All-time-high(daily avg.)', value: `$ ${cryptodetails?.allTimeHigh?.price && millify(cryptodetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+  ];
+
+  const genericStats = [
+    { title: 'Number Of Markets', value: cryptodetails?.numberOfMarkets, icon: <FundOutlined /> },
+    { title: 'Number Of Exchanges', value: cryptodetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    { title: 'Aprroved Supply', value: cryptodetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: `$ ${cryptodetails?.supply?.total && millify(cryptodetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: `$ ${cryptodetails?.supply?.circulating && millify(cryptodetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
+  ];
+
+  return (
+     <Col className="coin-detail-container">
+      <Col className="coin-heading-container">
+         <Title level={2} className="coin-name">
+          {cryptodetails.name} Price
+          {/* ({cryptoDetails.slug})  */}
+        </Title> 
+       <p>
+          {cryptodetails.name} live price in inr, view value statistics , market
+           cap and supply
+         </p>
+       </Col>  
+      <Select
+        defaultValue="7d"
+        className="select-timeperiod"
+        placeholder="select time period"
+        onChange={(value) => setTimePeriod(value)}
+      >
+        {time.map((date) => <Option key={date}>{date}</Option>)}
+      </Select>
+    </Col>
+  );
+};
+
+export default CryptoDetails;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,16 +1,15 @@
-import {createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': '27216bf07fmshdee129fa8936a8ep140b38jsn4c157ef95359',
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  "X-RapidAPI-Key": "27216bf07fmshdee129fa8936a8ep140b38jsn4c157ef95359",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
 
-}
-
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({
-    url, headers: cryptoApiHeaders
-})
+  url,
+  headers: cryptoApiHeaders,
+});
 
 // const options = {
 //     method: 'GET',
@@ -29,16 +28,22 @@ const createRequest = (url) => ({
 //       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 //     }
 //   };
-  
 
 export const cryptoApi = createApi({
-    reducerPath:'cryptoApi',
-    baseQuery:fetchBaseQuery({ baseUrl }),
-    endpoints:(builder) =>({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`) //
-        })
-    })
-})
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`), //
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+  }),
+});
 
-export const {useGetCryptosQuery,} = cryptoApi;
+export const { useGetCryptosQuery , useGetCryptoDetailsQuery ,useGetCryptoHistoryQuery } = cryptoApi;
